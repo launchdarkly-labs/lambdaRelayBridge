bdf48956cea5855d4d38a833b11a1c146906ba7e
Manuel Partida <manuel@launchdarkly.com>
2019-02-26 17:25:45 -0800

    uploading latest working version, w/callbacks

--- index.js
+++ index.js
@@ -1,29 +1,49 @@
-var LaunchDarkly = require('ldclient-node');
-//var redis = require('redis')
+const LaunchDarkly = require('ldclient-node');
+const atob = require('atob');
+const DynamoDBFeatureStore = require('ldclient-node-dynamodb-store');
+const dynamoDBOptions = { region: process.env.DYNAMO_REGION, accessKeyId: process.env.DYNAMO_ACCESS_KEY, secretAccessKey: process.env.DYNAMO_SECRET_KEY};
+const store = DynamoDBFeatureStore('ld-relay', { clientOptions: dynamoDBOptions, prefix:"ld:manuel_demo:production" });
+const config = {
+    featureStore: store,
+    useLdd: true
+  };
 
-exports.handler = (event, context, callback) => {
-    
-    const flagKey = event.pathParameters.flag;
-    const userBase64 = event.pathParameters.user;
-    const userStr = atob(userBase64);
-    const user = JSON.parse(userStr);
+let ldClient = null
+let isReady = false
 
-    //configure dynamoDB client
+function evaluate(event, context, callback) {
+  const flagKey = event.pathParameters.flag || "undefined";
+  const userBase64 = event.pathParameters.user;
+  const userStr = atob(userBase64);
+  const user = JSON.parse(userStr);
+  console.log("once ready called");
 
+  ldClient.variation(flagKey, user, false, function(err, variation) {
+    console.log("variation being performed");
+    if (variation === undefined) throw('unknown variation')
+    console.log("here is my variation: " + variation);
+    ldClient.flush();
+    callback(null, {"statusCode": 200, "body": JSON.stringify(variation)})
+    //callback(null, variation)
+  });
+}
 
-    // use ldd to connect to relay proxy
-
-
-    console.log("initialization")
-    ldclient = LaunchDarkly.init("sdk-8929dfb7-7edd-4bec-9856-db3bd80a6661", {
-        //feature_store: new LaunchDarkly.RedisFeatureStore(redisConfig),
-        //use_ldd: true
-    });
-    console.log("user")
-    
-    console.log("once")
-    ldclient.once('ready', () => {
-        console.log("ready")
-        ldclient.variation(flagKey, user, false, callback);
-    });
+exports.handler = function (event, context, callback) {
+  if (!event.pathParameters.flag) throw('missing flag')
+  if (!event.pathParameters.user) throw('missing user')
+  
+  context.callbackWaitsForEmptyEventLoop = false;
+  
+  if (!ldClient) {
+    ldClient = LaunchDarkly.init(process.env.SDK_KEY, config);
+  }
+  if (!isReady) {
+    ldClient.once(`ready`, function() {
+      isReady = true
+      evaluate(event, context, callback)
+    })
+  } else {
+    evaluate(event, context, callback)
+  }
 };
+
